f843169fce2fc5a46ec1a2d000d8d232d392c383
Kimson Wong <kimsonwong@gmail.com>
2020-05-06 21:19:45 +0800

    added initial layout for Login component

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -2,10 +2,24 @@ import React, {useState} from 'react';
 import './LogIn.scss';
 import Input, {ResultInterface} from '../elements/Input/Input';
 function LogIn() {
-    const [email, setEmail] = useState('');
+    const initialState = {value: '', valid: false, error: ''}
+    const [email, setEmail] = useState({...initialState});
+    const [password, setPassword] = useState({...initialState});
 
     const changeHandler = (result: ResultInterface) => {
-        setEmail(result.value);
+        const newState = {...result};
+        delete newState.origin
+
+        switch(result.origin) {
+            case 'email':
+                    setEmail(newState);
+                break;
+            case 'password':
+                    setPassword(newState);
+                break;
+            default:
+                throw new Error(`${result.origin} is not a valid origin`);
+        }
     };
 
     const formSubmitHandler = () => {
@@ -16,23 +30,42 @@ function LogIn() {
         <div className="LogIn">
             <div className="login--window padding-top-bottom-40 padding-left-right-20">
                 {/* {props.location.state ? <Notice text={props.location.state.message} /> : null} */}
-                {/* <img src={props.pageConfiguration.header.logo} style={{height: '70px'}} alt="logo" className="margin-bottom-20"/> */}
-                {/* <Heading level="two" alignment="center" text="Log in to your account" margin="margin-bottom-20" size="16px" color="var(--accent-four-shade-two)" /> */}
-                <h1 className="font-weight-400">Log In</h1>
+                <h1 className="main--heading margin-bottom-10">
+                    <span>Admin</span>
+                    <span>Desk</span>
+                </h1>
+                <h2 className="font-weight-300 margin-bottom-20" style={{fontSize: '22px'}}>Log in</h2>
                 <form onSubmit={formSubmitHandler}>
-                    <Input id="email" placeholder="Email" value={email} changeCallback={changeHandler} />
-                    {/*
+                    <Input 
+                        id="email" 
+                        placeholder="Email" 
+                        value={email.value} 
+                        changeCallback={changeHandler} 
+                        validatedProps={{email: true}} 
+                        valid={email.valid} 
+                        error={email.error} />
 
-                    <InputGroup
-                        change={handleChange}
-                        icon="ti-lock"
+                    <Input
                         id="password"
-                        origin="password"
                         placeholder="Password"
-                        type="password"
-                        margin="margin-top-20"
-                        validators={{}}
+                        value={password.value}
+                        changeCallback={changeHandler}
+                        valid={password.valid}
                     />
+
+                    <button style={{
+                        backgroundColor: 'var(--accent-three-shade-one)',
+                        color: '#fff',
+                        padding: '12px 10px',
+                        outline: 'none',
+                        fontSize: '14px',
+                        width: '100%',
+                        border: 'none',
+                        boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.2)'
+                    }}>
+                        Log in
+                    </button>
+                    {/*
                     <div className="block margin-top-20">
                         <CheckBox id="remember-me" text="Remember me" />
                         <Link to="/forgot-password" className="forgot--password--link">Forgot your password?</Link>
--- src/components/elements/Input/Input.tsx
+++ src/components/elements/Input/Input.tsx
@@ -1,23 +1,61 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 import './Input.scss';
 
 interface Props {
-    changeCallback(result: ResultInterface): void, 
-    id: string,
-    inputBackgroundColor?: string,
-    placeholder: string,
-    value: string
+    changeCallback(result: ResultInterface): void;
+    error?: string;
+    id: string;
+    inputBackgroundColor?: string;
+    margin?: string;
+    placeholder: string;
+    validatedProps?: {
+        email?: boolean
+    };
+    valid: boolean;
+    value: string;
 }
 
 function Input(props: Props) {
     const placeholderRef = useRef<HTMLSpanElement>(null!);
 
+    const validator = () => {
+        if (!props.validatedProps) return {valid: true, error: null};
+        type indexSignature = {
+            [key: string]: any
+        };
+        const result: indexSignature = {
+            email: {
+                valid: true,
+                errorMessage: "The email is invalid."
+            }
+        };
+        const predicate: indexSignature = {
+            email: () => {
+                const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+                return result['email'] = regex.test(props.value);
+            }
+        }
+
+        Object.entries(props.validatedProps).forEach(el => {
+            result[el[0]] = {...result[el[0]], valid: predicate[el[0]]()}
+        });
+        
+        return {
+            valid: Object.entries(result).map(e => e[1]['valid']).every(valid => valid),
+            error: (function() {
+                const error = Object.entries(result).map(e => e[1]).find(el => !el['valid']);
+                return error ? error['errorMessage'] : null;
+            })()
+        }
+    }
+
     const changeHandler = (inputValue: string) => {
-        props.changeCallback({value: inputValue, valid: true, origin: props.id})
+        const {valid, error} = validator();
+        props.changeCallback({value: inputValue, valid: valid, origin: props.id, error: error})
     }
 
     const focus = () => {
-        placeholderRef.current.style.transform = 'translateY(-35px) scale(0.8)';
+        placeholderRef.current.style.transform = 'translateY(-35px) scale(0.75)';
     }
 
     const blur = () => {
@@ -37,7 +75,10 @@ function Input(props: Props) {
                     style={{backgroundColor: props.inputBackgroundColor  || 'transparent'}}/>
                 <span ref={placeholderRef} className="label--placeholder">{props.placeholder}</span>
                 <span className="input--border"></span>
+                {/* <div className={`margin-6 input--group--error ${!props.valid ? 'visible' : ''}`} ref={inputError}>{props.error}</div> */}
             </label>
+            <div 
+                className="input--error" style={{margin: '3px 6px', visibility: !props.valid ? 'visible' : 'hidden'}}>{props.error}</div>
         </div>
     )
 }
@@ -45,7 +86,8 @@ function Input(props: Props) {
 export interface ResultInterface {
     origin: string, 
     valid: boolean,
-    value: string
+    value: string,
+    error: string
 }
 
 export default Input;
