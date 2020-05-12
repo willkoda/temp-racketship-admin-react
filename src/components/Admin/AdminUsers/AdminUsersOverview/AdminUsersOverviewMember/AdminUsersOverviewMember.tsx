adc184b3410cb7e5ab58ffa1dc94327405e9c56e
Kimson Wong <kimsonwong@gmail.com>
2020-05-12 16:19:17 +0800

    added club information and account details tp AdminUserOverviewMember

--- src/components/Admin/AdminUsers/AdminUsersOverview/AdminUsersOverviewMember/AdminUsersOverviewMember.tsx
+++ src/components/Admin/AdminUsers/AdminUsersOverview/AdminUsersOverviewMember/AdminUsersOverviewMember.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import Button from '../../../../../elements/Button/Button';
 
+interface UserOverview {
+    email: string;
+    first_name: string;
+    id: number;
+    last_name: string;
+    mobile_number: string;
+    role: string;
+    verified: boolean;
+    verified_on: string;
+    blocked: boolean;
+    blocked_on: string;
+    organization: {
+        identifier: string;
+        name: string;
+    }
+}
+
 interface Props {
-    memberInformation: Partial<{
-        email: string;
-        first_name: string;
-        id: number;
-        last_name: string;
-        mobile_number: string;
-        role: string;
-        verified: boolean;
-        verified_on: string;
-        blocked: boolean;
-        blocked_on: string;
-    }>;
+    memberInformation: Partial<UserOverview>;
 }
 
 function AdminUsersOverviewMember(props: Props) {
 
-    const renderAccountDetails = () => {
-        const accountDetails = [
-            'id', 'name', 'email', 'mobile_number', 'verified', 'verified_on', 'blocked', 'blocked_on'
-        ];
-
+    const renderElements = (params: {keys: Array<String>, propObject: Partial<UserOverview>}) => {
         return Object.entries(props.memberInformation).reduce((acc: Array<JSX.Element>, curr, index: number): Array<JSX.Element> => {
             const key = curr[0].split('_').join(' ');
-            if (accountDetails.includes(curr[0])) {
+            if (params.keys.includes(curr[0])) {
                 acc.push(
                     <div className="box--row" key={index}>
                         <div className="key">{key}:</div>
@@ -48,7 +50,10 @@ function AdminUsersOverviewMember(props: Props) {
                 <div className="box padding-bottom-10">
                     <h3 className="box--heading">Account Details</h3>
                     <div className="box--details">
-                        {renderAccountDetails()}
+                        {renderElements({
+                            keys: ['id', 'name', 'email', 'mobile_number', 'verified', 'verified_on', 'blocked', 'blocked_on'],
+                            propObject: props.memberInformation
+                        })}
                     </div>
                     <div className="detail--buttons">
                         <Button 
@@ -60,7 +65,21 @@ function AdminUsersOverviewMember(props: Props) {
                             width="80px" />
                     </div>
                 </div>
-            </div> 
+
+                <div className="box">
+                    <h3 className="box--heading">Club</h3>
+                    <div className="box--details">
+                        <div className="box--row">
+                            <div className="key">organization:</div>
+                            <div className="value">{props.memberInformation.organization?.name}</div>
+                        </div>
+                        {renderElements({
+                            keys: ['referral_code', 'total_cash_outs', 'total_purchases'],
+                            propObject: props.memberInformation
+                        })}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
