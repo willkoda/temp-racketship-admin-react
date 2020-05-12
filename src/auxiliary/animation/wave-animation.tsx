ce5c9c846b3ef1d4ec73c7d161ec5aba0cbb3aef
Kimson Wong <kimsonwong@gmail.com>
2020-05-12 14:00:34 +0800

    added wave--cover element to wave-animation to resolve click handler not executing when timing the event as the wave element is removed from the DOM

--- src/auxiliary/animation/wave-animation.tsx
+++ src/auxiliary/animation/wave-animation.tsx
@@ -8,10 +8,27 @@ export function waveAnimation(params: Params) {
     params.waveElementRef.current.style.position = 'relative';
     params.waveElementRef.current.style.overflow = 'hidden';
 
+    if (!params.waveElementRef.current.querySelector('.wave--cover')) {
+        const cover = document.createElement('span');
+        cover.classList.add('wave--cover');
+        cover.style.position = 'relative';
+        cover.style.display = 'block';
+
+        cover.style.width = params.waveElementRef.current.offsetWidth + 'px';
+        cover.style.height = params.waveElementRef.current.offsetHeight + 'px';
+        cover.style.zIndex = '1';
+        cover.style.position = 'absolute';
+        cover.style.top = '0px';
+        cover.style.left = '0px';
+        
+        params.waveElementRef.current.appendChild(cover);
+    }
+
     const waveElement = document.createElement('span');
     waveElement.style.width = params.waveElementRef.current.offsetWidth / 2 + 'px';
     waveElement.style.height = params.waveElementRef.current.offsetWidth / 2 + 'px';
     waveElement.style.backgroundColor = params.waveColor;
+    waveElement.style.zIndex = '0';
     waveElement.classList.add('wave--animation');
     waveElement.classList.add('--wave');
     params.waveElementRef.current.appendChild(waveElement);
